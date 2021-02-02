0fa3abfe59692618578c4e1551b2a9357566d2ad
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2021-02-02 17:23:35 +1100

    Fix conda channels.

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+"""A simple hello world Hail Batch example."""
+
 import os
-import hailtop.batch as hb 
+import hailtop.batch as hb
 
 backend = hb.ServiceBackend(
-    billing_project=os.getenv('HAIL_BILLING_PROJECT'),
-    bucket=os.getenv('HAIL_BUCKET'))
+    billing_project=os.getenv('HAIL_BILLING_PROJECT'), bucket=os.getenv('HAIL_BUCKET')
+)
 
-batch = hb.Batch(backend=backend, name='hello world') 
+batch = hb.Batch(backend=backend, name='hello world')
 
-job = batch.new_job(name='hello') 
-job.command('echo "hello world"') 
+job = batch.new_job(name='hello')
+job.command('echo "hello world"')
 
 batch.run()
